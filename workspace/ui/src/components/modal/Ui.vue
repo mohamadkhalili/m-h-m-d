27f761ff50b3239cacf7229fd546cb9cbf36eaaa
alireza_gh <alireza@alireza.ir>
2024-10-26 08:16:07 +0330

    add Ui.vue to modal

---
+++ workspace/ui/src/components/modal/Ui.vue
@@ -0,0 +1,11 @@
+<template>
+    <modal>
+        <template #close></template>
+        <template #default></template>
+    </modal>
+</template>
+
+<script setup lang="ts">
+import modal from './Core.vue';
+
+</script>
