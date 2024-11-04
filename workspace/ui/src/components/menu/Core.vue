c6f75628051565a5b11d9d06e9f4c275df7b038f
alireza_gh <alireza@alireza.ir>
2024-11-04 12:32:59 +0330

    firts commit for menu create menu component

---
+++ workspace/ui/src/components/menu/Core.vue
@@ -0,0 +1,5 @@
+<template>
+  <slot name="menu"></slot>
+</template>
+<script setup lang="ts">
+</script>
---
+++ workspace/ui/src/components/menu/Ui.vue
@@ -0,0 +1,8 @@
+<template>
+  <Core>
+    <template #menu> </template>
+  </Core>
+</template>
+<script setup lang="ts">
+import Core from "./Core.vue";
+</script>
