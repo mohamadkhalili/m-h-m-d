66d840907e1656fcc3c26f8b5485f1e438e95cad
alireza_gh <alireza@alireza.ir>
2024-11-05 12:40:45 +0330

    add emit to menu component

--- workspace/ui/src/components/menu/Core.vue
+++ workspace/ui/src/components/menu/Core.vue
@@ -1,6 +1,19 @@
 <template>
   <slot name="menu"></slot>
+  <template v-for="(item, index) in items" :key="index">
+    <div @click="setModelValue(item)">
+    </div>
+  </template>
 </template>
 <script setup lang="ts">
+import { defineProps } from 'vue';
+import { coreProps } from './Props';
+import { menuEmits } from './Emits';
+const props = defineProps(coreProps);
+const emit = defineEmits(menuEmits);
+
+const setModelValue = (temp: String) => {
+    emit("update:modelValue", temp);
+};
 
 </script>
