540c0b9ec6c4d2555f19b5e1a311004c5246b054
alireza_gh <alireza@alireza.ir>
2024-11-06 11:50:19 +0330

    fix problem for config tailwind

--- workspace/docs/tailwind.config.js
+++ workspace/docs/tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     './docs/**/*.{md,vue}',
-    './../components/src/**/*.{js,ts,vue,md}',
+    './../ui/src/**/*.{js,ts,vue,md}',
     './docs/.vitepress/**/*.{js,ts,vue}',
   ],
   theme: {
