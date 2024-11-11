bdb0dc613523875c8e1537b80c363f65c585d8c9
alireza_gh <alireza@alireza.ir>
2024-11-11 11:01:29 +0330

    update menu add background

--- workspace/ui/src/components/menu/Core.vue
+++ workspace/ui/src/components/menu/Core.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="{ 'vertical-menu': vertical }" @click="handleClick">
+  <div class="menu" :class="{ 'vertical-menu': vertical }" @click="handleClick">
     <div v-for="(item, index) in items" :key="index">
       <slot
         name="item"
@@ -17,10 +17,10 @@ import { defineProps } from "vue";
 import { coreProps } from "./Props";
 import { menuEmits } from "./Emits";
 import { coreSlots } from "./Slots";
+import { useBgColorClassName } from "../../composables/ColorComposable";
 const props = defineProps(coreProps);
 const emit = defineEmits(menuEmits);
 const slots = defineSlots<coreSlots>();
-
 const handleClick = (event: MouseEvent) => {
   const target = event.target as HTMLElement;
   if (target.classList.contains("menu-item")) {
