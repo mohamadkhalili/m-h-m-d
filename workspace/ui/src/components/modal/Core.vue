709327a981ab1ec0cc7a266bb15286c66619d98f
alireza_gh <alireza@alireza.ir>
2024-11-12 16:18:17 +0330

    fix problem for modal and modal.md

--- workspace/ui/src/components/modal/Core.vue
+++ workspace/ui/src/components/modal/Core.vue
@@ -5,7 +5,10 @@
       class="modal-overlay"
       @click.self="handleOutsideClick"
     >
-      <div class="modal-content">
+      <div
+        class="modal-content"
+        :class="[bgColorClass.active, textColorClass.active]"
+      >
         <slot name="close"></slot>
         <slot name="default"></slot>
       </div>
@@ -16,21 +19,26 @@
 <script setup lang="ts">
 import { modalSlots } from "./Slots";
 import { coreProps } from "./Props";
-import { defineProps, defineSlots, defineEmits } from "vue";
+import { defineProps, defineSlots, defineEmits, watchEffect } from "vue";
 import { modalEmits } from "./Emits";
+import {
+  useBgColorClassName,
+  useColorClassName,
+} from "../../composables/ColorComposable";
 
 const props = defineProps(coreProps);
 const slots = defineSlots<modalSlots>();
 const emit = defineEmits(modalEmits);
-
+const textColorClass = useColorClassName(props);
+const bgColorClass = useBgColorClassName(props);
 function handleOutsideClick() {
   if (props.closeOnOutside) {
     const modalOverlay = document.querySelector(".modal-overlay");
     if (modalOverlay) {
-      modalOverlay.classList.add("fadeOut"); 
+      modalOverlay.classList.add("fadeOut");
       setTimeout(() => {
         emit("update:modelValue", false);
-      }, 500); 
+      }, 500);
     }
   }
 }
