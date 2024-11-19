93fb762bd9516f36bd699cf3d230f271970fc077
alireza_gh <alireza@alireza.ir>
2024-11-19 17:30:54 +0330

    replace color to class for modal and outside of modal

--- workspace/m-h-m-d/src/components/modal/Core.vue
+++ workspace/m-h-m-d/src/components/modal/Core.vue
@@ -3,11 +3,12 @@
     <div
       v-if="modelValue"
       class="modal-overlay"
+      :class="`${outsideClass} bg-black bg-opacity-10 fixed inset-0 backdrop-blur-sm flex items-center justify-center opacity-0 z-[9999]`"
       @click.self="handleOutsideClick"
     >
       <div
         class="modal-content"
-        :class="[bgColorClass.active, textColorClass.active]"
+        :class="`${modalClass} bg-teal-900 text-white rounded-xl p-5 w-[400px] h-[200px] -translate-y-5 shadow-xl z-[1000]`"
       >
         <slot name="close"></slot>
         <slot name="default"></slot>
@@ -20,16 +21,9 @@
 import { modalSlots } from "./Slots";
 import { coreProps } from "./Props";
 import { modalEmits } from "./Emits";
-import {
-  useBgColorClassName,
-  useColorClassName,
-} from "../../composables/ColorComposable";
-
 const props = defineProps(coreProps);
 const slots = defineSlots<modalSlots>();
 const emit = defineEmits(modalEmits);
-const textColorClass = useColorClassName(props);
-const bgColorClass = useBgColorClassName(props);
 function handleOutsideClick() {
   if (props.closeOnOutside) {
     const modalOverlay = document.querySelector(".modal-overlay");
