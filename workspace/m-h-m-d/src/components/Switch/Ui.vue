7ffb11c4e6d1893b8614ff550f2f2b451dec1808
ali-alinejad <winford.op@gmail.com>
2024-12-01 15:03:38 +0330

    review Ui.vue

--- workspace/m-h-m-d/src/components/Switch/Ui.vue
+++ workspace/m-h-m-d/src/components/Switch/Ui.vue
@@ -9,7 +9,7 @@ const emit = defineEmits(['update:modelValue']);
 const switchClasses = computed(() => [
   'relative inline-flex items-center transition-colors duration-300 rounded-full',
   props.isDisabled ? 'cursor-not-allowed opacity-50' : 'cursor-pointer',
-   'flex justify-between items-center',
+  'flex justify-between items-center',
   props.size === 'sm' ? 'w-11 h-5' : '',
   props.size === 'md' ? 'w-14 h-7' : '',
   props.size === 'lg' ? 'w-16 h-8' : '',
@@ -30,22 +30,16 @@ const toggleClasses = computed(() => {
     'absolute rounded-full transition-transform duration-300 transform',
     props.isDisabled ? 'opacity-60' : 'active:scale-95',
     baseClass,
-    'bg-white'
+    'bg-white',
   ];
 });
 
-const getTranslateValue = (size) => {
-  const translationMap = {
-    sm: 'translateX(155%)',
-    md: 'translateX(160%)',
-    lg: 'translateX(110%)',
-    xl: 'translateX(130%)',
-  };
-
-
-
-  return translationMap[size] || 'translateX(100%)';
-};
+const getTranslateValue = (size) => ({
+  sm: 'translateX(155%)',
+  md: 'translateX(160%)',
+  lg: 'translateX(110%)',
+  xl: 'translateX(130%)',
+}[size] || 'translateX(100%)');
 
 const toggleSwitch = () => {
   if (!props.isDisabled) {
@@ -55,7 +49,7 @@ const toggleSwitch = () => {
 </script>
 
 <template>
-  <Core :modelValue="props.modelValue"  :isDisabled="props.isDisabled" :defaultChecked="props.defaultChecked" @update:modelValue="emit('update:modelValue', $event)">
+  <Core :modelValue="props.modelValue" :isDisabled="props.isDisabled" :defaultChecked="props.defaultChecked" @update:modelValue="emit('update:modelValue', $event)">
     <template #default="{ isActive, toggleSwitch }">
       <div :class="switchClasses" @click="toggleSwitch">
         <span :class="['absolute right-0 mr-1 text-xs text-white', props.size === 'lg' || props.size === 'xl' ? 'text-sm' : '']" v-if="!isActive">{{ props.label1 }}</span>
