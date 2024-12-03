cd462856a55e27f0b0016894e6d47075ec56e838
ali-alinejad <winford.op@gmail.com>
2024-12-03 12:14:03 +0330

    debug css

--- workspace/m-h-m-d/src/components/Loader/Ui.vue
+++ workspace/m-h-m-d/src/components/Loader/Ui.vue
@@ -1,23 +1,21 @@
 <template>
-  <Core v-if="isVisible" :class="[sizeClasses]">
-    <span v-if="variant === 'default'" :class="['loader', colorClass]"></span>
-    <span v-else-if="variant === 'spinner'" :class="['loader-spinner', colorClass]"></span>
-    <span v-else-if="variant === 'circle'" :class="['loader-circle', colorClass]"></span>
-    <div v-else-if="variant === 'dots'" :class="['loader-dots', colorClass]">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
-  </Core>
+  <Core
+    v-if="isVisible"
+    class="flex justify-center items-center"
+    :class="[sizeClasses]"
+  >
+  <span v-if="variant === 'default'" :class="['loader',props.color]"></span>
+    <span v-else-if="variant === 'spinner'" :class="['loader',colorClasses]-spinner"></span>
+    <span v-else-if="variant === 'circle'" :class="['loader',colorClasses]-circle"></span>
+
+    <div v-else-if="variant === 'dots'" :class="['loader',colorClasses]-dots"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+
+
+</Core>
+
 </template>
 
-<script setup>
+<script setup >
 import { computed } from 'vue';
 import { loaderProps } from './props';
 
@@ -36,104 +34,189 @@ const sizeClasses = computed(() => {
   }
 });
 
-const colorClass = computed(() => {
-  switch (props.color) {
-    case 'primary':
-      return 'border-primary';
-    case 'secondary':
-      return 'border-secondary';
-    case 'warning':
-      return 'border-warning';
-    case 'error':
-      return 'border-error';
-    case 'light':
-      return 'border-light';
-    case 'dark':
-      return 'border-dark';
-    default:
-      return 'border-default';
-  }
-});
 </script>
 
 <style scoped>
-.loader, .loader-spinner, .loader-circle, .loader-dots div {
+.loader-spinner {
   width: 3rem;
-  height: 3rem;
-  border-width: 5px;
-  border-style: solid;
-  border-color: transparent;
-  border-radius: 50%;
-  position: relative;
-  animation: spin 1s linear infinite;
+aspect-ratio: 1;
+border-radius: 50%;
+border: 5px solid #0000;
+border-right-color: #f3c443;
+position: relative;
+animation: l24 1s infinite linear;
 }
 
-.loader::before, .loader-spinner::before, .loader-circle::before {
+.loader-spinner:before,
+.loader-spinner:after {
   content: "";
-  box-sizing: border-box;
   position: absolute;
-  inset: 0;
+  inset: -4px;
   border-radius: 50%;
   border: inherit;
   animation: inherit;
+  animation-duration: 2s;
+}
+.loader-spinner:after {
+  animation-duration: 4s;
+}
+@keyframes l24 {
+  100% {transform: rotate(1turn)}
 }
 
-.border-primary {
-  border-right-color: #0ba131;
-}
 
-.border-secondary {
-  border-right-color: #af0baf;
-}
 
-.border-warning {
-  border-right-color: #e6b820;
-}
 
-.border-error {
-  border-right-color: #f81010;
-}
 
-.border-light {
-  border-right-color: #e4e4e4;
-}
-
-.border-dark {
-  border-right-color: #2e2e2e;
-}
-
-.border-default {
-  border-right-color: #1371ec;
-}
-
-@keyframes spin {
-  to {
-    transform: rotate(360deg);
-  }
-}
 
 .loader-dots {
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: repeat(3, 1fr);
-  gap: 12px;
-  row-gap: 0;
-  scale: 0.8;
-  justify-content: center;
-  align-items: center;
+display: grid;
+grid-template-columns: repeat(3, 1fr);
+grid-template-rows: repeat(3, 1fr);
+gap: 12px;
+row-gap: 0px;
+scale: 0.8;
+width: 72px;
+height: 72px;
+justify-content: center;
+align-items: center;
 }
 
 .loader-dots div {
-  background: currentColor;
-  animation: loader-dots 1.2s linear infinite;
+width: 12px;
+height: 12px;
+border-radius: 50%;
+background: currentColor;
+animation: loader-dots 1.2s linear infinite;
+}
+
+.loader-dots div:nth-child(1) {
+animation-delay: 0s;
+}
+.loader-dots div:nth-child(2) {
+animation-delay: -0.4s;
+}
+.loader-dots div:nth-child(3) {
+animation-delay: -0.8s;
+}
+.loader-dots div:nth-child(4) {
+animation-delay: -0.4s;
+}
+.loader-dots div:nth-child(5) {
+animation-delay: -0.8s;
+}
+.loader-dots div:nth-child(6) {
+animation-delay: -1.2s;
+}
+.loader-dots div:nth-child(7) {
+animation-delay: -0.8s;
+}
+.loader-dots div:nth-child(8) {
+animation-delay: -1.2s;
+}
+.loader-dots div:nth-child(9) {
+animation-delay: -1.6s;
 }
 
 @keyframes loader-dots {
-  0%, 100% {
-    opacity: 1;
-  }
-  50% {
-    opacity: 0.5;
-  }
+0%, 100% {
+  opacity: 1;
 }
+50% {
+  opacity: 0.5;
+}
+}
+
+
+
+
+.loader  {
+width: 3rem;
+scale: 1.3;
+height: 32px;
+
+    position: relative;
+    animation: rotate 1s linear infinite
+  }
+  .loader::before {
+    content: "";
+    box-sizing: border-box;
+    position: absolute;
+    inset:0px;
+    border-radius: 50%;
+    border: 3px solid #1371ec;
+    animation: prixClipFix 2s linear infinite ;
+  }
+
+  .secondary::before {
+
+    border: 3px solid #af0baf;
+
+  }
+
+  .primary::before {
+
+    border: 3px solid #0ba131;
+
+  }
+
+
+  .warning::before {
+
+    border: 3px solid #e6b820;
+
+  }
+
+  .error::before {
+
+    border: 3px solid #f81010;
+
+  }
+ 
+  .dark::before {
+    border: 3px solid #2e2e2e;
+  }
+
+  .light::before {
+
+    border: 3px solid #e4e4e4;
+
+  }
+
+
+
+  @keyframes rotate {
+    100%   {transform: rotate(360deg)}
+  }
+
+  @keyframes prixClipFix {
+      0%   {clip-path:polygon(50% 50%,0 0,0 0,0 0,0 0,0 0)}
+      25%  {clip-path:polygon(50% 50%,0 0,100% 0,100% 0,100% 0,100% 0)}
+      50%  {clip-path:polygon(50% 50%,0 0,100% 0,100% 100%,100% 100%,100% 100%)}
+      75%  {clip-path:polygon(50% 50%,0 0,100% 0,100% 100%,0 100%,0 100%)}
+      99% {clip-path:polygon(50% 50%,0 0,100% 0,100% 100%,0 100%,0 0)}
+      100%   {clip-path:polygon(50% 50%,0 0,0 0,0 0,0 0,0 0)}
+
+  }
+
+
+  .loader-circle {
+width: 3rem;
+position: absolute;
+height: 3rem;
+
+border-radius: 50%;
+border: 5px solid rgb(136, 135, 135); 
+border-top-color: rgb(52, 111, 201);
+animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+to {
+  transform: rotate(360deg);
+}
+}
+
+
 </style>
+
