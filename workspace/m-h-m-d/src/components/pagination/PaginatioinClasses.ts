88aaf19aed5ae0bd8eea2be95c3670bf5db8e6da
alireza_gh <alireza@alireza.ir>
2024-12-07 09:14:01 +0330

    all of classes in pagination update

--- workspace/m-h-m-d/src/components/pagination/PaginatioinClasses.ts
+++ workspace/m-h-m-d/src/components/pagination/PaginatioinClasses.ts
@@ -1,8 +1,8 @@
 export const paginationClasses = {
-    uiInputClass: "text-center flex justify-center items-center outline-none size-10 rounded-full bg-slate-200 text-black",
-    uiButtonClass: "w-10 h-10 rounded-full bg-slate-950 text-white flex items-center justify-center",
-    uiActiveClass: "cursor-pointer elevation-1 text-center flex justify-center items-center select-none size-10 rounded-full bg-slate-950 text-white cursor-pointer elevation-1",
-    uiOnActiveClass: "cursor-pointer elevation-1 text-center flex justify-center items-center select-none size-10 rounded-full bg-slate-200 text-black cursor-pointer elevation-1",
-    uiSeparatorClass: "select-none mx-3 text-black"
-  };
-  
+  uiContainerClass: "flex items-center gap-4 w-fit",
+  uiInputClass: "text-center flex justify-center items-center outline-none size-10 rounded-full bg-slate-200 text-black",
+  uiButtonClass: "w-10 h-10 rounded-full bg-slate-950 text-white flex items-center justify-center",
+  uiActiveClass: "cursor-pointer elevation-1 text-center flex justify-center items-center select-none size-10 rounded-full bg-slate-950 text-white cursor-pointer elevation-1",
+  uiOnActiveClass: "cursor-pointer elevation-1 text-center flex justify-center items-center select-none size-10 rounded-full bg-slate-200 text-black cursor-pointer elevation-1",
+  uiSeparatorClass: "select-none mx-3 text-black"
+};
