49ab30f0f3b4046e2dcfd27cb8809e14a9d3d59a
alireza_gh <alireza@alireza.ir>
2024-12-15 16:06:57 +0330

    change stepper for use adapter

---
+++ workspace/m-h-m-d/src/styles/StepperClasses.ts
@@ -0,0 +1,11 @@
+export const stepperClasses = {
+  uiProgressClass: "absolute top-8 h-1.5 m-0 shadow-none bg-gray-300 w-[95%] overflow-hidden",
+  uiProgressBarClass: "bg-green-500 h-full transition-all duration-300 ease-linear",
+  uiDoneClass: "bg-green-500 transition-all duration-500 delay-200 flex justify-center w-10 h-10 text-center p-2 text-white rounded-full select-none cursor-pointer items-center",
+  uiNotDoneClass: "bg-gray-300 p-2 text-white rounded-full cursor-pointer items-center flex justify-center w-10 h-10 select-none text-center",
+  generalClass: "flex flex-row justify-between relative pb-4 pt-4",
+  stepClass: "flex flex-col items-center z-20",
+  startStepClass: "flex flex-col items-start z-20",
+  endStepClass: "flex flex-col items-end z-20",
+};
+  
