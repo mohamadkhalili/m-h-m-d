8212b4bd8663cde9c5f94e940af2836f4525f026
ali-alinejad <winford.op@gmail.com>
2024-12-17 14:37:47 +0330

    update chat bubble

---
+++ workspace/m-h-m-d/src/styles/Chatbubble.ts
@@ -0,0 +1,21 @@
+export const ChatBubbleClasses = {
+    uiChatClass: " flex w-full p-2'",
+    uiRightSideClass: " justify-end",
+    uiLeftSideClass: " justify-start",
+
+    uiChatBubbleMainClass:'relative max-w-[75%]',
+    uiAvatarLeftSideClass:'-left-10 top-0  absolute',
+uiAvatarRightSideClass:'-right-10 top-0 absolute',
+UiChatPaddingLeftSideClass:'pl-12 pt-2',
+UiChatPaddingRightSideClass:'pr-12 pt-2',
+UiChatImageClass:'w-10 h-10 rounded-full border  border-gray-300 ',
+uiChatHeaderLeftClasses:'text-lg text-left justify-start flex-row ml-4',
+uiChatHeaderRightClasses:'text-lg text-right justify-start flex-row-reverse mr-4 ',
+uiChatSubHeaderSizeClasses:'text-xs px-2',
+
+
+uiChatMassageClass:'px-4 py-2 shadow-md max-w-[75%]',
+
+
+  };
+  
