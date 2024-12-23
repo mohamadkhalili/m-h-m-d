ee7173ceb08b771eb5044fa71cf6acde1424f370
alireza_gh <alireza@alireza.ir>
2024-12-23 11:17:25 +0330

    add slots in ui and props for ui

--- workspace/m-h-m-d/src/components/alert/Props.ts
+++ workspace/m-h-m-d/src/components/alert/Props.ts
@@ -1,4 +1,15 @@
+import * as mdiIcons from "@mdi/js";
+import { PropType } from "vue";
 export const coreProps = {
     modelValue: { type: Boolean, default: true },
-    
+    icon: { type: String as PropType<keyof typeof mdiIcons>, default: "" },
+    title: { type: String, default: "" },
+    text: { type: String, default: "" },
+}
+
+export const uiProps = {
+    modelValue: { type: Boolean, required: true },
+    icon: { type: String as PropType<keyof typeof mdiIcons>, required: false },
+    title: { type: String, required: false },
+    text: { type: String, required: false },
 }
---
+++ workspace/m-h-m-d/src/components/alert/Ui.vue
@@ -0,0 +1,12 @@
+<template>
+    <Core>
+        <template #alert>
+            <slot name="icon"></slot>
+            <slot name="title"></slot>
+            <slot name="text"></slot>
+        </template>
+    </Core>
+</template>
+<script setup lang="ts">
+import Core from "./Core.vue"
+</script>
