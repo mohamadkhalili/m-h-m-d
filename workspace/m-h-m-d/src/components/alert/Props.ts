ccdeb8bd2fb23d5a0c11e23415b2884624a43d5a
alireza_gh <alireza@alireza.ir>
2024-12-24 12:26:10 +0330

    add class in styles for alert component

--- workspace/m-h-m-d/src/components/alert/Props.ts
+++ workspace/m-h-m-d/src/components/alert/Props.ts
@@ -4,5 +4,5 @@ export const alertProps = {
     modelValue: { type: Boolean, default: true },
     icon: { type: String as PropType<keyof typeof mdiIcons>, default: "mdiAlertCircleOutline" },
     title: { type: String, default: "alert" },
-    text: { type: String, default: "text for alert" },
+    text: { type: String, default: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus! Eaque cupiditate minima, at placeat totam, magni doloremque veniam neque porro libero rerum unde voluptatem!" },
 }
---
+++ workspace/m-h-m-d/src/styles/AlertClasses.ts
@@ -0,0 +1,7 @@
+export const alertClasses = {
+    alertClass: "flex items-start p-4 bg-gray-100 border border-gray-300 rounded-lg shadow-md",
+    iconClass: "mr-4 mt-1 w-8 h-8 rounded-full text-emerald-600",
+    titelAndTextClass: "flex-1",
+    titleClass: "text-lg font-semibold text-gray-800",
+    textClass: "text-sm text-gray-600 mt-1",
+}
