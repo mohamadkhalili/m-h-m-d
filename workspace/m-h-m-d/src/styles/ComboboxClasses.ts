6343419c7ce836675f3be844f6aa8597a3e31fa8
alireza_gh <alireza@alireza.ir>
2025-01-04 13:23:42 +0330

    add class in styles and use adapter calss in combobox

---
+++ workspace/m-h-m-d/src/styles/ComboboxClasses.ts
@@ -0,0 +1,9 @@
+export const comboboxClasses = {
+    container: "w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus-within:ring focus-within:ring-gray-500 focus-within:border-gray-500 overflow-auto",
+    chipContainer: "flex flex-wrap gap-2 mb-1",
+    chip: "px-2 py-1 bg-gray-500 text-white rounded-full cursor-pointer",
+    chipClose: "ml-2 text-sm font-bold",
+    input: "w-full border-none focus:outline-none",
+    dropdownItem: "cursor-pointer px-4 py-2 hover:bg-gray-300 hover:text-black",
+    dropdownItemActive: "bg-gray-500 text-white",
+  };
