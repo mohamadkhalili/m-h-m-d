ba047e02a964232c57e7a1a99dbd0323690bbbe3
ali-alinejad <winford.op@gmail.com>
2025-01-20 10:12:22 +0330

    reformat design for button

--- workspace/m-h-m-d/src/styles/buttonClasses.ts
+++ workspace/m-h-m-d/src/styles/buttonClasses.ts
@@ -1,4 +1,4 @@
 export const ButtonClasses = {
-    uiButtonClass: "rounded-lg text-sm w-24 h-10 text-white transition-all duration-200 cursor-pointer z-10 overflow-hidden bg-gray-700",
+    uiButtonClass: "rounded-lg text-sm w-24 h-10 text-white transition-all duration-200 bg-slate-700 cursor-pointer z-10 overflow-hidden",
   };
   
