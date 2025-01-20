43d8d11d0816e7262fa7b12305a3f2cda75e0348
ali-alinejad <winford.op@gmail.com>
2025-01-20 10:25:28 +0330

    update input codes

--- workspace/m-h-m-d/src/styles/InputClasses.ts
+++ workspace/m-h-m-d/src/styles/InputClasses.ts
@@ -3,10 +3,10 @@ export const inputClass = {
     uiButtonClass: "absolute left-2 -translate-y-1/2 top-1/2  pr-8 ",
     uiButtonPrefixClass: "",
     uiButtonSuffixClass: "absolute right-3 -translate-y-1/2 top-1/2 p-1 ",
-    uiInputBaseClass: " px-10 py-3  transition-all duration-300 ",
-    UiLabelActiveClass:'top-[-25px]   text-gray-800',
-    UiLabelNotActiveClass:' bg-transparent top-3 text-gray-400 ',
-    UiInputActiveClass:'rounded-lg ring-2 ring-gray-600 text-gray-900 dark:text-gray-400',
-    UiInputNotActiveClass:'ring-2 ring-gray-400 rounded-lg p-2',
+    uiInputBaseClass: " px-10 py-3  transition-all duration-300 text-slate-500 ",
+    UiLabelActiveClass:'top-[-25px]   text-slate-500',
+    UiLabelNotActiveClass:' bg-transparent top-3 text-slate-500 ',
+    UiInputActiveClass:'rounded-lg ring-2   ',
+    UiInputNotActiveClass:'ring-2 ring-slate-400 rounded-lg p-2',
   };
   
