789bd8cdb4faae94ea0f901cd68ad6b7a4c6aa7c
alireza_gh <alireza@alireza.ir>
2025-01-20 13:06:24 +0330

    update alert class

--- workspace/m-h-m-d/src/styles/AlertClasses.ts
+++ workspace/m-h-m-d/src/styles/AlertClasses.ts
@@ -1,7 +1,7 @@
 export const alertClasses = {
-    alertClass: "flex items-start p-4 bg-gray-100 border border-gray-300 rounded-lg shadow-md",
-    iconClass: "mr-4 w-8 h-8 rounded-full text-gray-600",
+    alertClass: "flex items-start p-4 bg-slate-200 border border-slate-300 rounded-lg shadow-md",
+    iconClass: "mr-4 w-8 h-8 rounded-full text-slate-700",
     titelAndTextClass: "flex-1",
-    titleClass: "select-none text-lg font-semibold text-gray-800",
-    textClass: "select-none text-sm text-gray-600 mt-1",
+    titleClass: "select-none text-lg font-semibold text-slate-700",
+    textClass: "select-none text-sm text-slate-700 mt-1",
 }
