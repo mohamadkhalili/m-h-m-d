03d49fce84f0e8950b3b0b1da5caf4e387bfb9dc
alireza_gh <alireza@alireza.ir>
2025-01-20 13:52:09 +0330

    update date picker color

--- workspace/m-h-m-d/src/styles/BreadcrumbClasses.ts
+++ workspace/m-h-m-d/src/styles/BreadcrumbClasses.ts
@@ -1,7 +1,7 @@
 export const breadcrumbClasses = {
     generalClass: "inline-flex items-center space-x-2",
-    uiActiveClass: "inline-flex items-center space-x-2 cursor-pointer font-bold text-slate-700 hover:text-slate-600",
-    uiOnActiveClass: "inline-flex items-center space-x-2 cursor-pointer text-slate-500 hover:text-slate-600",
+    uiActiveClass: "inline-flex items-center space-x-2 cursor-pointer font-bold text-slate-700 hover:text-slate-600 transition duration-300",
+    uiOnActiveClass: "inline-flex items-center space-x-2 cursor-pointer text-slate-500 hover:text-slate-600 transition duration-300",
     uiDisableClass: "inline-flex items-center space-x-2 text-slate-300 cursor-not-allowed",
     separatorClass: "inline-flex items-center space-x-2 mx-1 text-xs text-slate-700",
 }
